b5a35157f2eaa8ddcc49f1d30f62d2051bba45d0
N103Gremillion <nathan103grem@gmail.com>
2024-09-02 21:12:11 -0500

    button stuff

---
+++ src/graphics/button.c
@@ -0,0 +1,73 @@
+#include "button.h"
+
+// function prototypes
+Button createButton(int x, int y, int width, int height){  
+
+    Button button;
+    SDL_Rect rect;
+
+    rect.x = x;
+    rect.y = y;
+    rect.w = width;
+    rect.h = height;
+    
+    button.rect = rect;
+
+    return button;
+
+}
+
+
+// so called setters
+void changeButtonWidth(Button* button, int width){
+    if (button != NULL && width <= 0){
+        button -> rect.w = width;
+    }
+}
+
+void changeButtonHeight(Button* button, int height){
+    if (button != NULL && height <= 0){
+        button -> rect.h = height;
+    }
+}
+
+void changeButtonX(Button* button, int x){
+    if (button != NULL){
+        button -> rect.x = x;
+    }
+}
+
+void changeButtonY(Button* button, int y){
+    if (button != NULL){
+        button -> rect.y = y;
+    }
+}
+
+
+// so called getters
+void getButtonWidth(const Button* button,int* width){    
+    if (button != NULL && width != NULL){
+        *width = button->rect.w;
+    }
+}
+
+void getButtongHeight(const Button* button, int* height){
+    if (button != NULL && height != NULL){
+        *height = button -> rect.h;
+    }
+}
+
+void getButtonX(const Button* button, int* x){
+    if (button != NULL && x != NULL){
+        *x = button -> rect.x;
+    }
+}
+
+void getButtonY(const Button* button, int* y){
+    if (button != NULL && y != NULL){
+        *y = button -> rect.y;
+    }
+}
+
+
+
---
+++ src/graphics/button.h
@@ -0,0 +1,28 @@
+#ifndef BUTTON_H
+#define BUTTON_H
+
+#include <SDL2/SDL.h>
+#include "color.h"
+
+// stuct wrapping SDL_Rect
+typedef struct{
+    SDL_Rect rect;
+} Button;
+
+// function prototypes
+Button createButton(int x, int y, int width, int height);
+
+// so called setters
+void changeButtonWidth(Button* button,int width);
+void changeButtonHeight(Button* button, int height);
+void changeButtonX(Button* button, int x);
+void changeButtonY(Button* button, int y);
+
+// so called getters
+void getButtonWidth(const Button* button,int* width);
+void getButtongHeight(const Button* button, int* height);
+void getButtonX(const Button* button, int* x);
+void getButtonY(const Button* button, int* y);
+
+
+#endif

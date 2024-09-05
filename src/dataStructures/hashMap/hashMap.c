7f7d2c96aab5bfcaf174512229665e27ac0652aa
N103Gremillion <nathan103grem@gmail.com>
2024-09-04 19:48:19 -0500

    function prototype for hashmap

--- src/dataStructures/hashMap/hashMap.c
+++ src/dataStructures/hashMap/hashMap.c
@@ -1,3 +1,7 @@
 #include "hashMap.h"
 
 
+
+
+
+
--- src/dataStructures/hashMap/hashMap.h
+++ src/dataStructures/hashMap/hashMap.h
@@ -1,9 +1,22 @@
 #ifndef HASHMAP_H
 #define HASHMAP_H
 
-typedef struct {
-    void** array;
-} hashMap;
+#include "../linkedList/linkedList.h"
 
+// this is set to a size of 151 and not dynamically allocated
+typedef struct {
+    LinkedNode** buckets;
+    int size;
+    int capacity;
+    float loadFactor;
+} HashMap;
+
+// prototypes
+void resizeMap(HashMap* map);
+void calcLoad(HashMap* map);
+void put(HashMap* map, char* key, void* value); 
+void* get(HashMap* map, char* key);
+void remove(HashMap* map, char* key);
+unsigned int hash(char* key, int capacity);
 
 #endif

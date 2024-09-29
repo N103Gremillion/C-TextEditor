11d0d44066ddfc5b5639426ff0f9af2b2631f9c2
N103Gremillion <nathan103grem@gmail.com>
2024-09-28 19:54:14 -0500

    I got it compiling with no error and basic logic down for the graphics of the characters

--- src/graphics/character.c
+++ src/graphics/character.c
@@ -5,19 +5,31 @@ Character initCharacter(SDL_Renderer* renderer, TTF_Font* font, char data, int x
 	character.data = data;
 	character.x = x;
 	character.y = y;
-	character.width = width;
-	character.height = height;
+	character.font = font;
 	character.color.r = 0;
 	character.color.g = 0;
 	character.color.b = 0;
 	character.color.a = 255;
+	
+	// create a temporary string for the char to calclate some values
+	char text[2] = {data, '\0'};
+	
+	// claculate the height and width
+	int width = 0, charHeight = 0;
+	// function that sets the width and charHeight using the font and string
+	TTF_SizeText(font, text, &width, &charHeight);
+	character.width = width;
+	character.height = charHeight;
+	 
 	SDL_Rect rect;
 	rect.x = x;
 	rect.y = y;
 	rect.w = width;
-	rect.h = height;
+	rect.h = charHeight;
 	character.rect = rect;
-	SDL_Texture* texture;
+	setCharTexture(renderer, character);
+	
+	return character;
 }
 
 Character* initCharacters(int numOfChars){
@@ -33,7 +45,9 @@ void increaseCharactersLength(Character** characters, int newSize){
 	*characters = newCharacters;
 }
 void setCharTexture(SDL_Renderer* renderer, Character character){
-	SDL_Surface* surface = TTF_RenderText_Solid(character.font, &(character.data), character.color);
+	char text[2] = {character.data, '\0'};
+	
+	SDL_Surface* surface = TTF_RenderText_Solid(character.font, text, character.color);
 	character.texture = SDL_CreateTextureFromSurface(renderer, surface);
 	SDL_FreeSurface(surface);
 }
